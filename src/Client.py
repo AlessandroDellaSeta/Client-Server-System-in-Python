c976189fa63e20c6725ce55ce1637d3e3a8cda3e
AlessandroDellaSeta <ale.dellaseta@gmail.com>
2018-11-16 14:22:26 -0500

    The Server can communicate with the Database. Implemented search employee and insert employee. To insert a new employee, the user must know the password.

--- src/Client.py
+++ src/Client.py
@@ -1,24 +1,61 @@
+# -*- coding: utf-8 -*-
+
 import zmq
 
 context = zmq.Context()
 
 #  Socket to talk to server
-print("Connecting to  server…")
+# print("Connecting to  server…")
 socket = context.socket(zmq.REQ)
 socket.connect("tcp://localhost:5555")
 
 
 while(True):
-	name = input("What is your name? ")
 
-	print("Sending request %s …")
+	#Select operation:
+	name = input("What operation? (i=insert; s=search) ")
 	socket.send_string(name)
-
-	#  Get the reply.
 	message = socket.recv()
 	message = message.decode("utf-8")
+	print(message)
 
-	print("Received reply %s " % (message))
+	if name == 's':
+		name = input("")
+		socket.send_string(name)
+		mess = socket.recv()
+		mess = mess.decode("utf-8")
+		print("Received reply %s " % mess)
+	elif name == 'i':
+		psw = input("")
+		socket.send_string(psw)
+		mess = socket.recv()
+		mess = mess.decode("utf-8")
+		print(mess)
+
+
+		if mess != "wrong password":
+			name = input("")
+			socket.send_string(name)
+			mess = socket.recv()
+			mess = mess.decode("utf-8")
+			print(mess)
+			lastname = input("")
+			socket.send_string(lastname)
+			mess = socket.recv()
+			mess = mess.decode("utf-8")
+			print(mess)
+			id = input("")
+			socket.send_string(id)
+			mess = socket.recv()
+			mess = mess.decode("utf-8")
+			print(mess)
+		else:
+			continue
+	else:
+		print("Wrong command. ")
+
+
+
 	inp = input("Continue? (y/n) ")
 	if inp != 'y':
 		break
--- src/Server.py
+++ src/Server.py
@@ -1,30 +1,85 @@
+import psycopg2 as psycopg2
 import zmq
 
 context = zmq.Context()
 socket = context.socket(zmq.REP)
 socket.bind("tcp://*:5555")
-database = {"name" : ["Alessandro", "Jack"],
-            "last name" : ["Della Seta", "Black" ],
-            "ID" : ["10429777","21223933"]}
 
-def search(dict, value):
-    for k in dict:
-        count = 0
-        for v in dict[k]:
-            if value in v:
-                return [dict['name'][count],dict['last name'][count],dict['ID'][count]]
-            count += 1
+def admincheck():
+    pass
 
-    return None
+def search(conn , cursor, value):
+    # execute  Query
+    temp =  value.upper()
+
+    val ="'" + '%'+temp+'%' + "'"
+    cursor.execute(""" SELECT * FROM "Employers" WHERE "Name" LIKE """ + val +
+                   """ OR "Last name" LIKE  """ + val +
+                   """ OR "ID" LIKE  """ + val)
+
+    # retrieve the records from the database
+    records = cursor.fetchall()
+    print (records)
+    if len(records)==0:
+        return "NOT FOUND"
+    return records
+
+def insert(conn , cursor, name, lastname, id):
+    query = ("""INSERT INTO public."Employers" ("Name", "Last name", "ID") VALUES (%s, %s, %s)""")
+    data = (name,lastname,id)
+    cursor.execute(query,data)
+    conn.commit()
+
 
 while True:
     #  Wait for next request from client
+    try:
+        conn = psycopg2.connect("dbname=postgres user=postgres password=gaara host=127.0.0.1")
+    except:
+        print ("IMPOSSIBLE to connect to the database")
+
+    #Define a cursor for database
+    cursor = conn.cursor()
+
     message = socket.recv()
     message = message.decode("utf-8")
     print("Received request: %s" % message)
+    mes = message.lower()
+    if mes == "s":
+        socket.send_string("Who are you looking for? ")
+        message = socket.recv()
+        message = message.decode("utf-8")
+        answer = search(conn,cursor,message)
+    elif mes == 'i':
+        socket.send_string("Please insert password: ")
+        psw = socket.recv()
+        psw = psw.decode("utf-8")
 
-    #  Do some 'work'
-    answer = search(database,message)
+        if psw == "ciao":
+            socket.send_string("Please insert name: ")
+            nm = socket.recv()
+            nm = nm.decode("utf-8")
+            name = nm.upper()
+
+            socket.send_string("Please insert last name: ")
+            ltnm = socket.recv()
+            ltnm = ltnm.decode("utf-8")
+            lastname = ltnm.upper()
+
+            socket.send_string("Please insert id: ")
+            idi = socket.recv()
+            idi = idi.decode("utf-8")
+            id = idi.upper()
+
+            insert(conn, cursor, name, lastname, id)
+            answer = "employer insert correctly"
+        else:
+            print("wrong psw")
+            answer = "wrong password"
+
+    else:
+        answer = ""
+    print('sent')
     sent = str(answer)
     # Send reply back to client
     socket.send_string(sent)
